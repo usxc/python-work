c4d5f5304561588fd3743a983bb2cf8b5e3ca625
usxc <tinypopopo@gmail.com>
2026-04-24 10:20:27 +0900

    feat: update code to chap3-15

--- pycharm/chapter3test/chap3-14.py
+++ pycharm/chapter3test/chap3-14.py
@@ -2,14 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.rcParams["font.family"] = "sans-serif"
-plt.rcParams["font.sans-serif"] = [
-    "Hiragino Maru Gothic Pro",
-    "Hiragino Sans",
-    "BIZ UDGothic",
-    "MS Gothic"
-]
+plt.rcParams["font.sans-serif"] = ["Hiragino Maru Gothic Pro",
+                                   "Hiragino sans", "BIZ UDGothic", "MS Gothic"]
 
-df = pd.read_csv("test.csv")
+# CSVファイルを読み込む（名前の列をインデックスで）
+df = pd.read_csv("test.csv", index_col=0)
 
-df.plot()
+# 棒グラフを作って表示する
+df.plot.bar()
+plt.legend(loc="lower right")
+plt.show()
+
+# 棒グラフ（水平）を作って表示する
+df.plot.barh()
+plt.legend(loc="lower left")
+plt.show()
+
+# 積み上げ棒グラフを作って表示する
+df.plot.bar(stacked=True)
+plt.legend(loc="lower right")
+plt.show()
+
+# 箱ひげグラフを作って表示する
+df.plot.box()
+plt.show()
+
+# 面グラフを作って表示する
+df.plot.area()
+plt.legend(loc="lower right")
 plt.show()
