61ff580b05b40ddd240da9882b8c290094ca91d0
usxc <tinypopopo@gmail.com>
2026-04-24 11:27:54 +0900

    feat: update code to chap3-19

--- pycharm/chapter3test/chap3-17.py
+++ pycharm/chapter3test/chap3-17.py
@@ -11,4 +11,5 @@ df = pd.read_csv("test.csv", index_col=0)
 colorlist = ["skyblue", "steelblue", "tomato", "cadetblue", "orange", "sienna"]
 df.T.plot.bar(color=colorlist)
 plt.legend(loc="lower right")
+plt.savefig("bargraph.png")
 plt.show()
