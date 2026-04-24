08495480dd1063c489c239b5956dfabf08ad192d
usxc <tinypopopo@gmail.com>
2026-04-24 10:39:22 +0900

    feat: update code to chap3-16

--- pycharm/chapter3test/chap3-14.py
+++ pycharm/chapter3test/chap3-14.py
@@ -8,26 +8,22 @@ plt.rcParams["font.sans-serif"] = ["Hiragino Maru Gothic Pro",
 # CSVファイルを読み込む（名前の列をインデックスで）
 df = pd.read_csv("test.csv", index_col=0)
 
-# 棒グラフを作って表示する
-df.plot.bar()
-plt.legend(loc="lower right")
-plt.show()
-
-# 棒グラフ（水平）を作って表示する
-df.plot.barh()
+# 国語の棒グラフ（水平）を作って表示する
+df["国語"].plot.barh()
 plt.legend(loc="lower left")
 plt.show()
 
-# 積み上げ棒グラフを作って表示する
-df.plot.bar(stacked=True)
-plt.legend(loc="lower right")
+# 国語と数学の棒グラフ（水平）を作って表示する
+df[["国語","数学"]].plot.barh()
+plt.legend(loc="lower left")
 plt.show()
 
-# 箱ひげグラフを作って表示する
-df.plot.box()
+# C子の棒グラフ（水平）を作って表示する
+df.loc["C子"].plot.barh()
+plt.legend(loc="lower left")
 plt.show()
 
-# 面グラフを作って表示する
-df.plot.area()
-plt.legend(loc="lower right")
+# C子の円グラフを作って表示する
+df.loc["C子"].plot.pie(labeldistance=0.6)
+plt.legend(loc="lower left")
 plt.show()
